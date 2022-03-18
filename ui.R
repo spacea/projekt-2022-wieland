16a9ec292dd55ccb56e73c7dcdf747330bd04261
Paul-inio <aleks.w2002@gmail.com>
2022-03-19 00:55:46 +0100

    poczatki webowej aplikacji

---
+++ ui.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Moja pierwsza aplikacja"),
+  sidebarLayout(
+    sidebarPanel(
+      p("Tutaj jest lewy panel")
+      
+    ),
+    mainPanel(
+      br(),
+      p("Tutaj jest prawy panel"),
+      textInput(inputId = "word", label = "Type a word.")
+    )
+  )
+))
