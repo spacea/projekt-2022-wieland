0e2629b3ab0f42e47c3ee9efe23fcbd0d4914d4e
Aleksander Wieland <101804150+Paul-inio@users.noreply.github.com>
2022-03-30 22:02:07 +0200

    teraz funkcja jest bardzo ladna

--- fndwords.R
+++ fndwords.R
@@ -1,58 +1,40 @@
-# potrzebny pakiet stringr
-
-# pckg_installed = function(){
-#   existence_pckg = require("stringr")
-#   if(existence_pckg == FALSE){
-#     install.packages("stringr")
-#     library(stringr)
-#   }
-# }
 install.packages("usethis")
 library(usethis)
 install.packages("available")
 library(available)
-
 available("fndwords")
 
-pckg_installed()
-preparation_folder = function(){
-if(dir.exists("dictionaries") == FALSE)
-{
-  dir.create("dictionaries")
-}}
+
+install.packages("stringr")
+library(stringr)
 
 
-download_words = function(dir_of_word, encoding_pck){
-words_together = readLines(dir_of_word, encoding = encoding_pck)
-words_together = tolower(words_together)
-
-  return(words_together)
-}
-
-# encoding: "iso8859-1", "UTF-8"
-
-find_words = function(write_a_word, dictionary, report){
+  find_words = function(write_a_word, dictionary, report = FALSE, to_low = TRUE){
+    if(is.vector(write_a_word) == FALSE){
+      stop("Argumentem dictionary musi byc wektor!")
+    }
   write_a_word = paste("^", write_a_word, "$", sep = "")
+  if(to_low == TRUE){
+    dictionary = tolower(dictionary)
+  }
   extracted_words = str_subset(dictionary, pattern = write_a_word)
-  print(extracted_words)
-  return(extracted_words)
-}
-
-report_txt = function(word_data){
-  get_words = word_data
-  first_word = word_data[1]
+  
+  if(report == TRUE){
+    first_word = extracted_words[1]
     if(dir.exists("results") == FALSE){
       dir.create("results")
     }
-    write.table(c(word_data), file = paste("results/", first_word, ".txt"), sep = "")
+    write.table(c(extracted_words), file = paste("results/", first_word, ".txt"), sep = "")
     cat("Report created successfully.")
     dir_return = paste(" Directory: results/", first_word, ".txt", sep = "")
     cat(dir_return)
+  }
+  cat("\n")
+  return(extracted_words)
 }
 
-# 
-# eng = readLines("C:/Users/User/Desktop/projekt_krzyzowkowy/dictionaries/english.txt")
-# # eng
-# # 
-# report_txt(find_words("si...",  eng))
 
+ 
+eng = readLines("C:/Users/User/Desktop/projekt_krzyzowkowy/dictionaries/english.txt")
+eng
+find_words("a..l", eng, report = TRUE)
